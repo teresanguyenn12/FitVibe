5891ab556c71ab108567dccfcb97ba2f6dc4c807
An Hanson <113488892+myan105@users.noreply.github.com>
2025-02-20 14:59:30 -0800

    Delete 10.6 directory

--- 10.6/10.6.py
+++
@@ -1,6 +0,0 @@
-# Two words are anagrams if you can rearrange the letters from one to spell the other.
-# Write a function called is_anagram that takes two strings and returns True if they are anagrams.
-
-
-def is_anagram(word1, word2)
-    return sorted(word1) == sorted(word2)
