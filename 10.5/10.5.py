9faaee04acf6ed6a5e4e79f680dc27aa2e12e1b5
An Hanson <113488892+myan105@users.noreply.github.com>
2025-02-20 14:59:22 -0800

    Delete 10.5 directory

--- 10.5/10.5.py
+++
@@ -1,7 +0,0 @@
-# Write a function called is_sorted that takes a list as a parameter and
-# returns True if the list is sorted in ascending order and False otherwise.
-
-
-def is_sorted(lst):
-    return lst == sorted(lst)
-
